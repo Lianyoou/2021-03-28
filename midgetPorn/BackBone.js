22ef3504f18ca5e0eb280a8243506f577a880363
Den <52953043+Lianyoou@users.noreply.github.com>
2022-04-06 12:32:31 +0200

    Add files via upload

---
+++ midgetPorn/BackBone.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+window.addEventListener("load", function(){
+  setTimeout(
+    function open(event){
+      document.querySelector("").style.display = "block";
+    }, 1000
+  )
+})
+*/
+
+
+
+
+/*const popup = document.querySelector(".div1");
+
+window.onload=function(){
+  setTimeout(function(){
+    popup.style.display ="block"
+  }, 2000)
+}
+*/
